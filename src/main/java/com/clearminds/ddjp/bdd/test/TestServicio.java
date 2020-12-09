265e806726687b131b24bc6d1441231c140100d9
Daniel Jimenez <danieldavidjp@gmail.com>
2020-12-09 11:36:07 -0500

    Implementacion de metodo insertar estudiante en Servicio Estudiantes y referenciacion de model en pom.xml + testServicio

---
+++ src/main/java/com/clearminds/ddjp/bdd/test/TestServicio.java
@@ -0,0 +1,21 @@
+package com.clearminds.ddjp.bdd.test;
+
+import com.clearminds.ddjp.dtos.Estudiante;
+import com.clearminds.ddjp.excepciones.BDDException;
+import com.clearminds.ddjp.servicios.ServicioEstudiante;
+
+public class TestServicio {
+	
+	public static void main(String[] args) {
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		
+		try {
+			srvEstudiante.insertarEstudiante(new Estudiante("Juan","Perez"));
+		} catch (BDDException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			e.getMessage();
+		}
+	}
+
+}
--- src/main/java/com/clearminds/ddjp/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/ddjp/servicios/ServicioEstudiante.java
@@ -1,5 +1,18 @@
 package com.clearminds.ddjp.servicios;
 
+
+
+import com.clearminds.ddjp.dtos.Estudiante;
+import com.clearminds.ddjp.excepciones.BDDException;
+
 public class ServicioEstudiante extends ServicioBase {
+	
+	public void insertarEstudiante(Estudiante estudiante) throws BDDException{
+		
+		abrirConexion();
+		System.out.println("ingresando estudiante "+estudiante.toString());
+		cerrarConexion();
+		
+	}
 
 }
