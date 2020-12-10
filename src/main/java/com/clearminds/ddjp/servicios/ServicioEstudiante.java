b2bc9a0bcb17116f1b708d18ab39e29c38da5c5a
Daniel Jimenez <danieldavidjp@gmail.com>
2020-12-10 09:55:19 -0500

    Se modifica la clase ServicioEstudiante para agregar al query de sql la edad

--- src/main/java/com/clearminds/ddjp/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/ddjp/servicios/ServicioEstudiante.java
@@ -13,12 +13,12 @@ public class ServicioEstudiante extends ServicioBase {
 		abrirConexion();
 		System.out.println("ingresando estudiante " + estudiante.toString());
 		Statement stmt = null;
-
+		
 		try {
 			stmt = getConexion().createStatement();
 
-			String sql = "insert into estudiantes(nombre,apellido) values('" + estudiante.getNombre() + "','"
-					+ estudiante.getApellido() + "')";
+			String sql = "insert into estudiantes(nombre,apellido,edad) values('" + estudiante.getNombre() + "','"
+					+ estudiante.getApellido() + "',"+estudiante.getEdad() + ")";
 
 			System.out.println("Script: " + sql);
 
@@ -42,7 +42,7 @@ public class ServicioEstudiante extends ServicioBase {
 			stmt = getConexion().createStatement();
 
 			String sql = "update estudiantes set nombre='" + estudiante.getNombre() + "', apellido='"
-					+ estudiante.getApellido() + "' where id ="+estudiante.getId();
+					+ estudiante.getApellido() + "',edad="+estudiante.getEdad()+ "where id ="+estudiante.getId();
 
 			System.out.println("Script: " + sql);
 
